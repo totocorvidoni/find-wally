6fbecd5c870865eca3a0b06b0cc9b4e42df0d492
Toto Corvidoni <totocorvidoni@gmail.com>
2019-03-07 16:25:10 -0300

    Initialize Repository.

---
+++ wally-front/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <play-area></play-area>
+    <sidebar></sidebar>
+  </div>
+</template>
+
+<script>
+import playArea from "./components/play-area";
+import sidebar from "./components/sidebar";
+
+export default {
+  name: "App",
+  components: {
+    playArea,
+    sidebar
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
