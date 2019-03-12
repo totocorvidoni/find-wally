1f9261a2d4ec0439bf6cba595877a22e9cf6a78d
Toto Corvidoni <totocorvidoni@gmail.com>
2019-03-12 14:24:31 -0300

    Set up front-end to recieve requests from back-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'game/index'
   get 'game/wally'
+  get 'game/wenda'
+  get 'game/woof'
+  get 'game/wbeard'
+  get 'game/odlaw'
 
   root :to => 'game#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
